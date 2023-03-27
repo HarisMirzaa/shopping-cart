c876ca900a0302a9101d063d1e7d98ab509a3c82
HarisMirzaa <127094041+HarisMirzaa@users.noreply.github.com>
2023-03-27 14:06:32 +0500

    updated

--- src/componenets/Nabar.tsx
+++ src/componenets/Nabar.tsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 import React from "react";
-// import { FiShoppingCart } from "react-icons/fi";
 import { ShoppingCart } from "tabler-icons-react";
-// import { useRecoilState } from "recoil";
-// import { cartState } from "../atoms/cartState";
+
 
 
 const Navbar = () => {
-//   const [cartItem] = useRecoilState(cartState);
 
   return (
     <section className="shadow-xl bg-white sticky top-0">
@@ -21,7 +18,7 @@ const Navbar = () => {
             <div>
               <ShoppingCart size={48} strokeWidth={2} color={"black"} />;
               <span className="absolute -top-2 -right-2 text-[13px] bg-red-600 h-[18px] w-[18px] rounded-full grid place-items-center text-white">
-                {/* {cartItem.length} */}0
+              0
               </span>
             </div>
           </Link>
--- src/componenets/Product.tsx
+++ src/componenets/Product.tsx
@@ -1,26 +1,8 @@
 import React from "react";
-// import { useRecoilState } from "recoil";
-// import { cartState } from "../atoms/cartState";
-// import toast from "react-hot-toast";
+
 
 const Product = ({ product }: any) => {
-  //   const [cartItem, setCartItem] = useRecoilState(cartState);
-
-  //   const addItemsToCart = () => {
-  //     if (cartItem.findIndex((pro) => pro.id === product.id) === -1) {
-  //       setCartItem((prevState) => [...prevState, product]);
-  //     } else {
-  //       setCartItem((prevState) => {
-  //         return prevState.map((item) => {
-  //           return item.id === product.id
-  //             ? { ...item, quantity: item.quantity + 1 }
-  //             : item;
-  //         });
-  //       });
-  //     }
-
-  //     toast(`${product.name} added to cart`);
-  //   };
+  
 
   return (
     <div className="bg-[#fff] pt-6 pb-4 shadow-2xl">
@@ -38,7 +20,6 @@ const Product = ({ product }: any) => {
 
         <button
           className="bg-red-600 text-white py-4 px-12 mt-4 block mx-auto hover:bg-red-800"
-          //   onClick={addItemsToCart}
         >
           Add To Cart
         </button>
